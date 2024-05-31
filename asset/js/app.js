a1fcba00f5fe414d59dae8fbf77e99f1a707c9e8
Md Shariful Islam <116838065+mrsharifull@users.noreply.github.com>
2024-06-01 00:28:39 +0600

    Firs Commit

---
+++ asset/js/app.js
@@ -0,0 +1,8 @@
+
+// var myCarousel = document.querySelector('#carouselExampleIndicators');
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//     interval: 2000,
+//     ride: 'carousel'
+// });
+
+UIkit.slider(element).startAutoplay();
