f5b6d05850bb08d55f7b142aaa6bd57d5d6ac6c0
shariful <116838065+mrsharifull@users.noreply.github.com>
2024-06-01 07:05:29 +0600

    Updated

--- asset/js/app.js
+++ asset/js/app.js
@@ -8,20 +8,17 @@ function ut() {
 
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('faq-title') ) {
-    // Remove 'angle-active' class from all spans
+  
     document.querySelectorAll('span.angle-active').forEach(function(span) {
       span.classList.remove('angle-active');
     });
 
-    // Remove 'faq-desc' class from all elements
     document.querySelectorAll('.faq-desc').forEach(function(desc) {
       desc.classList.remove('faq-desc-active');
     });
 
-    // Add 'angle-active' class to span inside clicked .faq-title
     event.target.querySelector('span').classList.add('angle-active');
 
-    // Add 'faq-desc' class to next .faq-desc after clicked .faq-title
     var nextDesc = event.target.nextElementSibling;
     if (nextDesc && nextDesc.classList.contains('faq-desc')) {
       nextDesc.classList.add('faq-desc-active');
